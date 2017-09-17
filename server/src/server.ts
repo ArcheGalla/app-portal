6a9aeecfec9ed692da9bdbefbbc538e2ded57605
artem <artempushkar@gmail.com>
2017-09-18 00:17:23 +0300

    add working sample of feathers js service

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,26 @@
-import * as feathers  from 'feathers';
-import {Request, Response} from "express";
-const rest = require('feathers-rest');
-const socketio = require('feathers-socketio');
-const memory = require('feathers-memory');
-const bodyParser = require('body-parser');
-const handler = require('feathers-errors/handler');
+import * as feathers from 'feathers';
+import * as rest from 'feathers-rest';
+import * as memory from 'feathers-memory';
+import * as bodyParser from 'body-parser';
+import * as socketIO from 'feathers-socketio';
+import * as handler from 'feathers-errors/handler';
+import * as favicon from 'serve-favicon';
+import {join} from 'path';
 
-// A Feathers app is the same as an Express app
-const app:feathers.Application = feathers();
+const app: feathers.Application = feathers();
 
-app.use('/', function(req, res){
-    res.json({"test":"test"}).status(200)
+app.use(favicon(join(__dirname, '../../client/src/assets/favicon.ico')));
+// app.use('/', function (req, res) {
+//     res.json({status: 200}).status(200);
+// });
+
+app.configure(rest());
+app.configure(socketIO());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/messages', memory());
+app.use(handler());
+
+app.listen(3000, function () {
+    console.log('Application started  on port 3000');
 });
-
-// Add REST API support
-// app.configure(rest());
-// Configure Socket.io real-time APIs
-// app.configure(socketio());
-// Parse HTTP JSON bodies
-// app.use(bodyParser.json());
-// Parse URL-encoded params
-// app.use(bodyParser.urlencoded({ extended: true }));
-// Register our memory "messages" service
-// app.use('/messages', memory());
-// Register a nicer error handler than the default Express one
-// app.use(handler());
-// Start the server
-app.listen(3000);
